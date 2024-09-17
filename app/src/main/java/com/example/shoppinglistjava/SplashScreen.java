5e64f07ace1e11da9b55df4ebb4cb2e465e132a3
aviraj <panchotiyaaviraj@gmail.com>
2024-09-17 11:35:05 +0530

    Add SplashScreen

---
+++ app/src/main/java/com/example/shoppinglistjava/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.shoppinglistjava;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.imageView);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.cart_anim);
+        imageView.startAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+    }
+}
